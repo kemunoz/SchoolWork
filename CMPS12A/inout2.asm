27ee8a85c1d5eab1d2a8948c93dfc2afa47393d4
Kevin Munoz <kevinmunoz@eduroam-169-233-237-105.ucsc.edu>
2018-02-12 21:45:21 -0800

    read and output string

--- CMPS12A/inout2.asm
+++ CMPS12A/inout2.asm
@@ -3,13 +3,28 @@
 #This program promots the user for an integer and prints it out
 .text
 main:
-	#prompt user for integer
+	#prompt user to enter a string
 	li $v0, 4
 	la $a0, prompt
 	syscall
-	#read the integer
+	#Read string
+	li $v0, 8
+	la $a0, input
+	la $a1, inputSize
+	syscall
+	#output the text
+	li $v0, 4
+	la $a0, output
+	syscall
+	#Print the string
+	li $v0, 4
+	la $a0, input
+	syscall
 	#end the program
 	li $v0, 10
 	syscall
-.data
-prompt: .asciiz "Please enter an integer: "
+.data 
+prompt: .asciiz "Please enter a string"
+input: .word 16
+inputSize: .word 15
+output: .asciiz "You entered: "
